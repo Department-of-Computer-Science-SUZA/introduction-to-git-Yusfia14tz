6ebdf4184d6bb9bc7d4c4378d1586e63206f9fb1
yusfia yussuf <yusfiayussuf8@gmail.com>
2024-04-23 12:08:29 +0300

    index

--- first.java
+++ first.java
@@ -2,7 +2,7 @@
 
 <body>
     <h1>my name is yusfia yussuf ali, iam student of state university of zanzibar, i study informaton technology applications and management!</h1>
-    <h1>my name is yusfia yussuf ali, i learn bitam , second year student!</h1>
+    <i learn bitam , second year student!</h1>
 </body>
 
 </html>
